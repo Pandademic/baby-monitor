1168782dc681c182f335aa51fdcc712a8a988aad
Pandademic <74566464+Pandademic@users.noreply.github.com>
2022-01-15 17:19:27 -0500

    Update main.js

--- main.js
+++ main.js
@@ -23,8 +23,6 @@ function gotResult(error,results){
           if(results[0] != "person"){
                document.getElementById("header").innerHTML="BABY LEFT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
                document.getElementById("title").innerHTML="! BABY gone !"
-               alarm = new Audio('alarm.mp3');
-               alarm.play();
           }
    }
 }
