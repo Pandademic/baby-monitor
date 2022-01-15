422392492540439486a6e39017bc62d22ee9a46e
Pandademic <74566464+Pandademic@users.noreply.github.com>
2022-01-15 17:31:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 camera="";
-status="";
 function setup(){
       camera=createCapture(VIDEO);
+      camera.hide();
       canvas=createCanvas();
       canvas.center();
-      objectDetector=ml5.objectDetector('cocossd',modelLoaded)
+      objectDetector=ml5.objectDetector('cocossd',modelLoaded);
 }
 function draw(){
     image(camera,0,0,480,380);
 }
 function modelLoaded(){
     console.log("Coco is here for Babysitting dutys");
-    status=true;
     objectDetector.detect(camera,gotResult);
 }
 function gotResult(error,results){
@@ -22,11 +21,11 @@ function gotResult(error,results){
         console.log(results);
           if(results[0] != "person"){
                document.getElementById("header").innerHTML="BABY LEFT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
-               document.getElementById("title").innerHTML="! BABY gone !"
+               document.getElementById("title").innerHTML="! BABY gone !";
           }
           else {
               document.getElementById("header").innerHTML="Nothing abnormal";
-              document.getElementById("title").innerHTML="Everything's OK"
+              document.getElementById("title").innerHTML="Everything's OK";
           }
    }
 }
